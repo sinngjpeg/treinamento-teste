46c3fda08273960bbc0f4acf37bd3c2754736869
Ingrid Silva <ingridsilvatec@outlook.com>
2020-12-29 09:12:12 -0300

    upercase, lowcase()

--- testestreinamento/app/src/main/java/com/sinngjpeg/testes_treinamento/PasswordValidator.kt
+++ testestreinamento/app/src/main/java/com/sinngjpeg/testes_treinamento/PasswordValidator.kt
@@ -1,8 +1,28 @@
 package com.sinngjpeg.testes_treinamento
 
+import java.util.*
+
 class PasswordValidator {
     fun isValid(password: String): Boolean {
         return password.length >= 8
 
     }
+
+    fun upperCase(password: String) : Boolean {
+        val separado = password.split(" , ").toTypedArray()
+        val mapeado = separado.map { item -> item == item.toUpperCase(Locale.ROOT)
+        }
+        return mapeado.contains(true)
+    }
+
+
+    fun lowerCase(password: String) : Boolean {
+        val separado = password.split(" , ").toTypedArray()
+        val mapeado = separado.map { item -> item == item.toLowerCase(Locale.ROOT)
+        }
+        return mapeado.contains(true)
+    }
+
+
+
 }
--- testestreinamento/app/src/test/java/com/sinngjpeg/testes_treinamento/PasswordValidatorTest.kt
+++ testestreinamento/app/src/test/java/com/sinngjpeg/testes_treinamento/PasswordValidatorTest.kt
@@ -12,9 +12,53 @@ class PasswordValidatorTest {
         //arrange
 
         //act
-        val result = passwordValidator.isValid("1234567")
+        val result = passwordValidator.isValid("1B3@f67")
 
         //assert
         Assert.assertFalse(result)
     }
+
+
+    @Test
+    fun givenPasswordWhithoutUppercase_whenValidate_shouldReturnFalse() {
+        //arrange
+
+        //act
+        val result = passwordValidator.upperCase("1B3@f67")
+
+        //assert
+        Assert.assertFalse(result)
+
+    }
+
+
+    @Test
+    fun givenPasswordWhithoutLowercase_whenValidate_shouldReturnFalse() {
+        //arrange
+
+        //act
+        val result = passwordValidator.lowerCase("1B3@f67")
+
+        //assert
+        Assert.assertFalse(result)
+
+    }
+
+    @Test
+    fun givenPasswordWhithoutNumber_whenValidade_shouldReturnFalse() {
+
+        //arrange
+        //act
+        //assert
+    }
+
+    @Test
+    fun givenPasswordWhithoutChar_whenValidade_shouldReturnFalse() {
+
+        //arrange
+        //act
+        //assert
+    }
 }
+
+
